bf65b94898b0d73a435eff1a2bad640379c055bf
Bagharia <hu.alexis@hotmail.com>
2024-12-12 14:25:21 +0100

    up

--- template/artiste.php
+++ template/artiste.php
@@ -1,3 +1,27 @@
+<?php
+  session_start();
+  require '../db.php';
+
+  $stmt = $db->prepare("SELECT 
+    artist.id AS artist_id,
+    CONCAT(artist.firstName, ' ', artist.lastName) AS artist_name,
+    AVG(avis.note) AS avg_rating
+  FROM 
+    avis
+  JOIN 
+    spectacles_parisiens ON avis.idSpectacle = spectacles_parisiens.id
+  JOIN 
+    artist ON spectacles_parisiens.id = artist.spectacle_id
+  GROUP BY 
+    artist.id
+  ORDER BY 
+    avg_rating DESC;
+    ");
+    $stmt->execute();
+    $artists = $stmt->fetchAll();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,13 +52,13 @@
               <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=2">17ème</a></li>
               <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=3">5ème</a></li>
               <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=4">1ème</a></li>
-              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=4">10ème</a></li>
-              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=4">14ème</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=5">10ème</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=6">14ème</a></li>
             </ul>
           </div>
           <a href="room.php" class="font-semibold hover:underline">Salle</a>
-          <a href="#" class="font-semibold hover:underline">Artiste</a>
-          <a href="#" class="font-semibold hover:underline">Les mieux notés</a>
+          <a href="artiste.php" class="font-semibold hover:underline">Artiste</a>
+          <a href="theatre.php" class="font-semibold hover:underline">Les mieux notés</a>
       </div>
       <div class="flex items-center gap-4">
         <input
@@ -47,7 +71,29 @@
         </a>
     </nav>
     <div class="border-t-2 border-gray-200 mt-2"></div>
-  </header>
+</header>
+<main>
+  <div class="flex justify-center mt-4">
+    <h1 class="text-lg font-semibold text-gray-900">Vos Meilleur Artistes</h1>
+  </div>
+  <div class="flex flex-wrap justify-between gap-6 m-6">
+    <?php foreach ($artists as $artist): ?>
+      <div class="w-[400px] bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden p-6">
+        <h1 class="text-center text-lg font-semibold text-gray-900">
+          <?php echo htmlspecialchars($artist['artist_name']); ?>
+        </h1>
+        <div class="p-4">
+          <div class="flex justify-center items-center">
+            <h2 class="text-lg font-semibold text-gray-900">Note Moyenne</h2>
+          </div>
+          <p class="text-center font-semibold text-gray-900 text-sm mt-2">
+            <?php echo number_format($artist['avg_rating'], 2); ?>
+          </p>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</main>
 </body>
 <script src="index.js"></script>
 </html>
---
+++ template/theatre.php
@@ -0,0 +1,112 @@
+<?php
+    session_start();
+    require '../db.php';
+
+    $sortOrder = isset($_GET['sort']) && $_GET['sort'] === 'desc' ? 'DESC' : 'ASC';
+
+    $stmt = $db->query("SELECT 
+        theatre.id AS theater_id, 
+        theatre.name AS theater_name, 
+        theatre.presentation,
+        AVG(avis.note) AS avg_rating
+    FROM 
+        theatre
+    JOIN 
+        room ON theatre.id = room.theater_id
+    JOIN 
+        representation ON room.id = representation.room_id
+    JOIN 
+        spectacles_parisiens ON representation.spectacle_id = spectacles_parisiens.id
+    JOIN 
+        avis ON spectacles_parisiens.id = avis.idSpectacle
+    GROUP BY 
+        theatre.id
+    ORDER BY 
+        avg_rating DESC;
+    ");
+    $theaters = $stmt->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../output.css"> 
+    <link rel="stylesheet" href="./input.css">
+</head>
+<body>
+<header>
+    <nav class="flex items-center justify-between p-4 relative">
+      <div class="flex items-center gap-10 ">
+        <a href="index.php" class="text-xl font-bold text-black">Scriptacle</a>
+        <div class="relative">
+          <a href="#" class="font-semibold hover:underline cursor-pointer" id="toggleCategories">Catégories</a>
+            <ul id="categoriesMenu" class="absolute left-0 top-10 bg-white shadow-lg rounded-lg hidden z-10">
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="category.php?category_id=1">Théâtre</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="category.php?category_id=2">Spectacle</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="category.php?category_id=3">Concert</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="category.php?category_id=4">Humour</a></li>
+            </ul>
+        </div>
+        <div class="relative">
+          <a href="#" class="font-semibold hover:underline" id="toggleArrondissement">Arrondissement</a>
+            <ul id="arrondissementMenu" class="absolute left-0 top-10 bg-white shadow-lg rounded-lg hidden z-10">
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=1">11ème</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=2">17ème</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=3">5ème</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=4">1ème</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=5">10ème</a></li>
+              <li class="px-4 py-2 hover:bg-gray-100"><a href="arrondissement.php?arrondissement_id=6">14ème</a></li>
+            </ul>
+          </div>
+          <a href="room.php" class="font-semibold hover:underline">Salle</a>
+          <a href="artiste.php" class="font-semibold hover:underline">Artiste</a>
+          <a href="theatre.php" class="font-semibold hover:underline">Les mieux notés</a>
+      </div>
+    
+      <div class="flex items-center gap-4">
+        <input
+          type="text"
+          placeholder="Rechercher"
+          class="p-2 w-64 rounded-md bg-gray-100 text-black"
+        />
+        <a href="login.html" class="bg-blue-600 text-white font-bold py-2 px-4 rounded-md">
+          Connexion
+        </a>
+    </nav>
+    <div class="border-t-2 border-gray-200 mt-2"></div>
+  </header>
+  <main>
+    <div class="flex flex-wrap justify-between gap-6 m-6">
+        <?php foreach ($theaters as $theater): ?>
+        <div class="w-[400px] bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden p-6">
+            <img 
+                src="https://d1k4bi32qf3nf2.cloudfront.net/thumb@3x/product/2024/06/spectaculaire_1719221025.jpg.webp" 
+                alt="<?php echo htmlspecialchars($theater['theater_name']); ?>" 
+                class="h-[200px] w-full object-cover rounded-lg !important">
+            <h1 class="text-center text-lg font-semibold text-gray-900">
+                <?php echo htmlspecialchars($theater['theater_name']); ?>
+            </h1>
+            <div class="p-4">
+                <div class="flex justify-center items-center">
+                    <h2 class="text-lg font-semibold text-gray-900">Présentation</h2>
+                </div>
+                <p class="text-center font-semibold text-gray-900 text-sm mt-2">
+                    <?php echo htmlspecialchars($theater['presentation']); ?>
+                </p>
+                <div class="mt-4 flex justify-center">
+                    <p class="text-sm font-semibold">Note Moyenne: <?php echo number_format($theater['avg_rating'], 2); ?></p>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</main>
+
+
+
+</body>
+<script src="index.js"></script>
+</html>
